6f0956f27da357510707cfb76e50aa4b668baf3f
Дмитрий <success-gala@rambler.ru>
2024-11-19 10:52:16 +0300

    Создана функция exchange для получения из интернета курсов валют

--- 9.3_02 Курсы валют с интефейсом.py
+++ 9.3_02 Курсы валют с интефейсом.py
@@ -4,6 +4,27 @@ import pprint
 from tkinter import *
 from tkinter import messagebox as mb
 
+from bottle import response
+
+
+def exchange():
+    code=entry.get().upper()
+    if code:
+        try:
+            response=requests.get('https://github.com/Gala-2072/Obmen.git')
+            response.raise_for_status()
+            data=response.json()
+            if code in data['rates']:
+                exchange_rate=data['rates'][code]
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate} {code} за 1 доллар")
+            else:
+                mb.showerror("Ошибка!", f"Валюта {code} не найдена!")
+        except Exception as e:
+            mb.showerror("Ошибка!", f"Произошла ошибка: {e}")
+    else:
+        mb.showwarning("Внимание!", "Введите код валюты!")
+
+
 
 window=Tk()
 window.title("Курсы обмена валют")
