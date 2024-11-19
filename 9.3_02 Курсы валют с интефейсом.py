a488878a05c519b84b3e82d2d440e314958bf62b
Дмитрий <success-gala@rambler.ru>
2024-11-19 13:00:35 +0300

    Вместо поля ввода добавлен выпадающий список.

--- 9.3_02 Курсы валют с интефейсом.py
+++ 9.3_02 Курсы валют с интефейсом.py
@@ -1,22 +1,21 @@
+from tkinter.ttk import Combobox
+
 import requests
 import json
-import pprint
 from tkinter import *
 from tkinter import messagebox as mb
-
-from bottle import response
-
+from tkinter import ttk
 
 def exchange():
-    code=entry.get().upper()
+    code=combobox.get().upper()
     if code:
         try:
-            response=requests.get('https://github.com/Gala-2072/Obmen.git')
+            response=requests.get(f'https://open.er-api.com/v6/latest/USD')
             response.raise_for_status()
             data=response.json()
             if code in data['rates']:
                 exchange_rate=data['rates'][code]
-                mb.showinfo("Курс обмена", f"Курс: {exchange_rate} {code} за 1 доллар")
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {code} за 1 доллар")
             else:
                 mb.showerror("Ошибка!", f"Валюта {code} не найдена!")
         except Exception as e:
@@ -24,18 +23,16 @@ def exchange():
     else:
         mb.showwarning("Внимание!", "Введите код валюты!")
 
-
-
 window=Tk()
 window.title("Курсы обмена валют")
 window.geometry("360x180")
+Label(text="Выберите код валюты:").pack(padx=10, pady=10)
+cur=['RUB', 'EUR', 'GBP', 'JPY', 'CNY', 'KZT', 'UZS', 'CHF', 'AED', 'CAD']
+combobox=ttk.Combobox(values=cur)
+combobox.pack(padx=10, pady=10)
 
-
-Label(text="Введите код валюты").pack(padx=10, pady=10)
-
-entry=Entry()
-entry.pack(padx=10, pady=10)
-
+#entry=Entry()# поле ввода теперь не нужно
+#entry.pack(padx=10, pady=10)
 Button(text="Получить курс обмена валюты", command=exchange).pack(padx=10, pady=10)
 
 window.mainloop()
